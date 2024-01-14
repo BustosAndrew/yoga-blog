9ef6fe1b2a81b30c63c50712622f360dc5e6fc51
Andrew Bustos <infernious@live.com>
2024-01-14 02:38:13 -0800

    config url change

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://yoga-blog-phi.vercel.app/",
+  site: "https://yogalightwave.com/",
   integrations: [
     tailwind(),
     image({
